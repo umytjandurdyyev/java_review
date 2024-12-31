91e8249ce40423466de20bb66c272a4cf274f319
Umytjan Durdyyev <udurdiyev@mail.ru>
2024-12-31 15:10:21 +0100

    BigDecimal

--- streams/src/main/java/com/cydeo/BigDecimalDemo.java
+++ streams/src/main/java/com/cydeo/BigDecimalDemo.java
@@ -9,11 +9,11 @@ public class BigDecimalDemo {
 
         double d1 = 174.56;
         double d2 = 174.26;
-        System.out.println(d1-d2);
+        System.out.println("Double: " + (d1-d2));
 
         BigDecimal b1 = new BigDecimal("174.56");
         BigDecimal b2 = new BigDecimal("174.26");
-        System.out.println(b1.subtract(b2));
+        System.out.println("BigDecimal: " + b1.subtract(b2));
 
         //Scaling and Rounding
         BigDecimal number1 = new BigDecimal(23.12);
